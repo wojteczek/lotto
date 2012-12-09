51c647f2f1b421ff04b3ec700030696344167467
Maciek Pacut <maciek.pacut@gmail.com>
2012-12-09 14:56:09 +0100

    Random unique numbers generation

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -2,6 +2,19 @@ class NumbersController < ApplicationController
   def show
     authenticate_user!
 
-    @numbers = [1,2,3,4,5,6]
+    today = Time.now
+
+    srand(current_user.id + today.year + today.month + today.day)
+    @numbers = rand_n(6, 1, 49)
+  end
+
+private
+
+  def rand_n(n, a, b)
+    randoms = Set.new
+    loop do
+      randoms << a + rand(b - a)
+      return randoms.to_a if randoms.size >= n
+    end
   end
 end
