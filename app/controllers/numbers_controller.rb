c57cf1d59cfbaa186f727ad9efe851839896b6c1
Maciek Pacut <maciek.pacut@gmail.com>
2012-12-09 14:17:11 +0100

    Displaying 6 numbers

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,5 +1,7 @@
 class NumbersController < ApplicationController
   def show
     authenticate_user!
+
+    @numbers = [1,2,3,4,5,6]
   end
 end
--- spec/features/numbers_spec.rb
+++ spec/features/numbers_spec.rb
@@ -8,9 +8,11 @@ describe 'Numbers' do
     sign_up_as_example_user
   end
 
-  it 'should show 6 number' do
-    page.should have_content('show')
+  it 'should show numbers page to logged in user' do
+    page.should have_content("Your lucky numbers are:")
   end
+
+  it 'should show 6 numbers'
   it 'should show numbers from range 1 to 49'
   it 'should show unique numbers'
 end
